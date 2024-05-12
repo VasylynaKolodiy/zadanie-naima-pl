cda5a8488dcfaf4e2118dc6aacca1f85bd66bc46
velisssa <tybinka.vasylyna@gmail.com>
2024-05-12 23:15:32 +0200

    Add facade

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,132 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
+const STATUS = {
+    available: 'available',
+    reserved: 'reserved',
+    sold: 'sold',
+};
+
+const data = [
+    {
+        lot: 1,
+        pieces: "4.5 + JH",
+        etage: "Rez-de-Jardin",
+        hab: 113.5,
+        balcon: 23.3,
+        terrasse: 72.2,
+        statut: STATUS.reserved,
+    },
+
+    {
+        lot: 2,
+        pieces: "3.5",
+        etage: "Rez-de-Jardin",
+        hab: 86.9,
+        balcon: 19.0,
+        terrasse: 44.2,
+        statut: STATUS.available,
+    },
+
+    {
+        lot: 3,
+        pieces: "4.5 + JH",
+        etage: "Rez-de-Jardin",
+        hab: 113.7,
+        balcon: 23.3,
+        terrasse: 72.2,
+        statut: STATUS.sold,
+    },
+
+    {
+        lot: 4,
+        pieces: "4.5 + JH",
+        etage: "Attique",
+        hab: 108.9,
+        balcon: 35.8,
+        terrasse: '',
+        statut: STATUS.available,
+    },
+
+    {
+        lot: 5,
+        pieces: "3.5 + JH",
+        etage: "Attique",
+        hab: 84.4,
+        balcon: 32.8,
+        terrasse: '',
+        statut: STATUS.available,
+    },
+
+    {
+        lot: 6,
+        pieces: "4.5 + JH",
+        etage: "Attique",
+        hab: 109.1,
+        balcon: 35.8,
+        terrasse: '',
+        statut: STATUS.available,
+    },
+];
+
+function handleHeaderOnScroll() {
+    $(window).scroll(function () {
         if ($(window).scrollTop() > 0) {
             $(".header").addClass("fixed");
         } else {
             $(".header").removeClass("fixed");
         }
     });
-});
+}
 
-$('.slider').slick({
-    slidesToShow: 0.6,
-    slidesToScroll: 1,
-    // autoplay: true,
-    autoplaySpeed: 2000,
-    centerMode: true,
-    centerPadding: '33%',
+function initializeSlider() {
+    $('.slider').slick({
+        slidesToShow: 1.8,
+        slidesToScroll: 1,
+        infinite: false,
+        autoplaySpeed: 2000,
+    });
+}
+
+function fillTableWithData(data) {
+    const tbody = $('#table tbody');
+    data.forEach(rowData => {
+        const row = $('<tr></tr>');
+        Object.entries(rowData).forEach(([key, value]) => {
+            const unit = (key === 'hab' || key === 'balcon' || key === 'terrasse') ? 'm<sup>2</sup>' : '';
+            row.append(`<td>${value} ${value ? unit : ''}</td>`);
+        });
+        applyStatusClassToLot(rowData);
+        tbody.append(row);
+    });
+}
+
+function applyStatusClassToLot(rowData) {
+    const lotNumber = rowData.lot;
+    const status = rowData.statut;
+    $(`.lot-${lotNumber}`).addClass(status);
+}
+
+function handleTableHover() {
+    $('#table tbody tr').hover(function() {
+        const lotNumber = $(this).find('td:first').text();
+        $(`.lot-${lotNumber}`).addClass('hover');
+    }, function() {
+        $('.facade__img svg path').removeClass('hover');
+    });
+}
+
+function handleSvgPathHover() {
+    $('.facade__img svg path').hover(function() {
+        console.log(5656)
+        const lotNumber = $(this).data('lot');
+        $(`.lot-${lotNumber}`).addClass('hover');
+    }, function() {
+        $('#table tbody tr').removeClass('hover');
+    });
+}
+
+$(document).ready(function() {
+    handleHeaderOnScroll();
+    initializeSlider();
+    fillTableWithData(data);
+    handleTableHover();
+    handleSvgPathHover();
 });
